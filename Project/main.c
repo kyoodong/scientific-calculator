eaf0171d9549f6ef6ed48dea365dd33b687fb28e
WindArrow <wind.dong.dream@gmail.com>
2016-05-21 23:15:28 +0900

    Initial Commit

---
+++ Project/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Project
+//
+//  Created by 이동규 on 2016. 5. 21..
+//  Copyright © 2016년 이동규. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
